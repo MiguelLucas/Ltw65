4d623233251099c994e3ce08a1f71ab4dba9ddb7
Miguel Lucas <cmiguel.lucas@gmail.com>
2015-11-17 13:02:03 +0000

    changed script

--- teste/script.js
+++ teste/script.js
@@ -7,21 +7,25 @@ function loadDocument() {
 }
 
 function loadEvents() {
+	alert('Hello 3');
   $.getJSON("events.php", eventsLoaded);
 }
 
 function eventsLoaded(data) {
+	alert('Hello 2');
   $.each(data, insertEvent);
   
   // Saves the first line for easy cloning
   //savedLine = $("#products .line:first-child").clone();
 }
 
-function insertEvent(key, event) {
+function insertEvent(key, evento) {
   // Create the option tag for the product
   var event = $("<option></option>");
-  event.text(event.name);
-  event.val(event.description);
+  alert('Hello');
+  //console.log(event.text(evento));
+  event.text(evento);
+  event.val(evento);
   
   // Insert the option tag in the select
   $("#events .line:first-child select").append(event);
