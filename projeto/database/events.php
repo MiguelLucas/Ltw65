d532bca7c1d7600fa21644424ff1e724d6b3fbb2
marianaowen <marianaowen@users.noreply.github.com>
2015-11-17 11:42:06 +0000

    getAllEvents
    function get all events.
    if request = GET, execute

--- projeto/database/events.php
+++ projeto/database/events.php
@@ -2,5 +2,15 @@
 	include_once('connection.php');
 
 	/* get all events */
-
+	function getAllEvents($dbh) {
+		$stmt = $db->prepare('SELECT * FROM Event');
+  		$stmt->execute();  
+  		$events = $stmt->fetchAll();
+  		echo json_encode($events);
+	}
+	
+	
+	if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+		getAllEvents();
+	}
 ?>
