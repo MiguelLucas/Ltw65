5d84513c3c7534fdb0d065a8431f2e2ac80a6f44
Miguel Lucas <cmiguel.lucas@gmail.com>
2015-11-19 16:56:18 +0000

    changed testeRegistration

--- testeRegistration/registration.php
+++ testeRegistration/registration.php
@@ -9,15 +9,25 @@ function createUser() {
 		$first_name = $_POST['firstName'];
 		$last_name = $_POST['lastName'];
 		$email = $_POST['email'];
-		$birth_date = $_POST['birthDate'];
+		$birth_day = $_POST['day'];
+		$birth_month = $_POST['month'];
+		$birth_year = $_POST['year'];
 		$password = $_POST['password'];
-		//$photo = $_POST['email'];
+		
+		$birth_date = $birth_year . "-" . $birth_month . "-" . $birth_day;
+		//$photo = $_POST['photo'];
 
 		$query = "INSERT INTO User (firstName, lastName, birthDate, email, password ) 
-		VALUES ( '$firstName', '$lastName', '$birthDate', '$email', '$password')";
+		VALUES ( '$first_name', '$last_name', '$birth_date', '$email', '$password')";
 		
 		$stmt = $db->prepare($query);
-		$stmt->execute();  
+		$result = $stmt->execute();  
+	
+		if ($result){
+		  echo "<p>You have been successfully registered!</p>";
+		} else {
+		  echo "<p>Sorry, there has been a problem inserting your details.</p>";
+		}
 	}
 }
 
