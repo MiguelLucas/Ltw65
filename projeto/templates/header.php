55a833e16a3e713eabc3ddde83cd38b6ddc8dc5c
marianaowen <marianaowen@users.noreply.github.com>
2015-12-03 10:53:01 +0000

    Minor fixes

--- projeto/templates/header.php
+++ projeto/templates/header.php
@@ -5,14 +5,13 @@
 	<meta charset='UTF-8'>
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<!-- Sweet Alert library (available at: https://t4t5.github.io/sweetalert/) -->
-	<script src="lib/swal/sweetalert.min.js"></script>
+	<script src="libs/swal/sweetalert.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="libs/swal/sweetalert.css">
 	<!-- Moments.js library (available at: http://momentjs.com/) -->
 	<script type="text/javascript" src="libs/moment.min.js"></script>
 	<!-- Our own scripts and styles -->
-	<script type="text/javascript" src="scripts.js"></script>
-	<script type="text/javascript" src="events.js"></script>
-	<script type="text/javascript" src="registration.js"></script>
+	<script type="text/javascript" src="scripts/events.js"></script>
+	<script type="text/javascript" src="scripts/registration.js"></script>
 	<!-- <link rel="stylesheet" href="style.css"> -->
 	<style type="text/css"> form input, textarea, form select {display: block;} .hidden {display: none;}</style>
 </head>
